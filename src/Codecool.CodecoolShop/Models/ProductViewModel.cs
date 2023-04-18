999e43ec6fff1443d9cdc844bb61396dccfb461c
MaciejTrebacz <maciekdmd90@gmail.com>
2023-04-18 19:39:04 +0700

    SortByProduct DONE

---
+++ src/Codecool.CodecoolShop/Models/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class ProductViewModel
+    {
+        public ProductCategory? ProductCategory { get; set; }
+        public List<Product> Products { get; set; }
+
+        public int IndexOf(Product element)
+        {
+            return Products.IndexOf(element);
+        }
+    }
+}

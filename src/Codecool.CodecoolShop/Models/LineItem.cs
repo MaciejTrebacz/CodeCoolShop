c08b9783580588addbb62b1774992cfe4e3db28d
Ostin-Oskarose <oskarcraft@gmail.com>
2023-04-18 13:10:03 +0200

    Added LineItem class

---
+++ src/Codecool.CodecoolShop/Models/LineItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class LineItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
